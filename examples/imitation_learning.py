89a62cd08b8f0792229ed0da01105f9e091c0b24
Wen Jiahao <wenjiahao@buaa.edu.cn>
2025-03-10 20:48:50 +0800

    Fix ValueError in imitation learning example for ragged arrays

--- examples/imitation_learning.py
+++ examples/imitation_learning.py
@@ -36,7 +36,7 @@ task = env.get_task(ReachTarget)
 il = ImitationLearning()
 
 demos = task.get_demos(2, live_demos=live_demos)  # -> List[List[Observation]]
-demos = np.array(demos).flatten()
+demos = np.array(demos, dtype=object).flatten()
 
 # An example of using the demos to 'train' using behaviour cloning loss.
 for i in range(100):
